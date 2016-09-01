d75328de30b2f2fe9a2df14f91e5700ccd6676fe
onewhaleid <dan.howe.mail@gmail.com>
2016-09-01 15:47:22 +1000

    region indices are now included

--- voronoi_polygons.py
+++ voronoi_polygons.py
@@ -2,96 +2,111 @@ import numpy as np
 from scipy.spatial import Voronoi, voronoi_plot_2d
 import matplotlib.pyplot as plt
 
-points = [
-[-2, 0],
-[2, 0],
-[0, 3],
-[0, 1.5],
-[0, -2],
-[-2, -1.5],
-[2, -1.5],
- ]
 
-points = np.array(points)
-vor = Voronoi(points)
+def finite_voronoi(vor, scale=1):
+    """
+    finite_voronoi(vor, scale=1)
 
-ax = plt.axes()
+    Construct finite boundaries for Voronoi diagram.
 
-#    ax.plot(vor.vertices[simplex,0], vor.vertices[simplex,1], 'r-')
+    Parameters
+    ----------
+    points : ndarray of floats, shape (npoints, 2)
+        Coordinates of points to construct a convex hull from
+    scale : float, optional
+        Scale of extension lines. Some points may be outside their associated
+        region if scale < 1
 
-ptp_bound = vor.points.ptp(axis=0).max()
+    Attributes
+    ----------
+    points : ndarray of double, shape (npoints, 2)
+        Coordinates of input points.
+    vertices : ndarray of double, shape (nvertices, 2)
+        Coordinates of the Voronoi vertices.
+    ridge_points : ndarray of ints, shape (nridges, 2)
+        Indices of the points between which each Voronoi ridge lies.
+    ridge_vertices : list of list of ints, shape (nridges, *)
+        Indices of the Voronoi vertices forming each Voronoi ridge.
+    regions : list of list of ints, shape (nregions, *)
+        Indices of the Voronoi vertices forming each Voronoi region.
+    point_region : list of ints, shape (npoints)
+        Index of the Voronoi region for each input point.
+    """
 
-# Count number of new vertices required
-nv_existing = len(vor.vertices)
-nv_required = len([v for v in vor.ridge_vertices if v[0] < 0])
-new_vertices = list(range(nv_existing, nv_existing + nv_required))
+    vor = Voronoi(points)
 
-outer_vertices = []
+    # Calculate size of extension lines
+    ptp_bound = vor.points.ptp(axis=0).max() * scale
 
-center = vor.points.mean(axis=0)
-for idx, pointidx, simplex in zip(range(len(vor.ridge_points)), vor.ridge_points, vor.ridge_vertices):
-    simplex = np.asarray(simplex)
-    if np.all(simplex >= 0):
-       ax.plot(vor.vertices[simplex,0], vor.vertices[simplex,1], '-')
-    else:
-        # First point
-        i = simplex[simplex >= 0][0]  # finite end Voronoi vertex
-        t = vor.points[pointidx[1]] - vor.points[pointidx[0]]  # tangent
-        t /= np.linalg.norm(t)
-        n = np.array([-t[1], t[0]])  # normal
+    # Count number of new vertices required
+    nv_existing = len(vor.vertices)
+    nv_required = len([v for v in vor.ridge_vertices if v[0] < 0])
+    new_vertices = list(range(nv_existing, nv_existing + nv_required))
+    outer_vertices = []
 
-        midpoint = vor.points[pointidx].mean(axis=0)
-        direction = np.sign(np.dot(midpoint - center, n)) * n
-        far_point = vor.vertices[i] + direction * ptp_bound
+    center = vor.points.mean(axis=0)
+    for idx, pointidx, simplex in zip(range(len(vor.ridge_points)), vor.ridge_points, vor.ridge_vertices):
+        simplex = np.asarray(simplex)
+        if np.any(simplex < 0):
 
-        ax.plot([vor.vertices[i,0], far_point[0]],
-                [vor.vertices[i,1], far_point[1]], 'k--')
-                
-        ax.text(far_point[0], far_point[1], pointidx)
+            # Find finite end Voronoi vertex
+            i = simplex[simplex >= 0][0]
 
-        outer_vertices.append(list(pointidx))
-        
-        # Add start point to ridge vertices
-        vor.ridge_vertices[idx][0] = len(vor.vertices)
-        vor.vertices = np.vstack([vor.vertices, far_point])
+            # Calculate tangent and normal
+            t = vor.points[pointidx[1]] - vor.points[pointidx[0]]
+            t /= np.linalg.norm(t)
+            n = np.array([-t[1], t[0]])
 
-# Assign points to outer vertices
-outer_points = list(set([item for sublist in outer_vertices for item in sublist]))
+            # Draw line
+            midpoint = vor.points[pointidx].mean(axis=0)
+            direction = np.sign(np.dot(midpoint - center, n)) * n
+            far_point = vor.vertices[i] + direction * ptp_bound
+            outer_vertices.append(list(pointidx))
 
-for op in outer_points:
-    p = [nv for nv, ov in zip(new_vertices, outer_vertices) if op in ov]
-    vor.ridge_vertices = np.vstack(
-        [vor.ridge_vertices, p])
-    vor.ridge_points = np.vstack(
-        [vor.ridge_points, [op, -1]])
+            # Add start point to ridge vertices
+            vor.ridge_vertices[idx][0] = len(vor.vertices)
+            vor.vertices = np.vstack([vor.vertices, far_point])
 
+    # Assign points to outer vertices
+    outer_points = list(set([item for sublist in outer_vertices for item in sublist]))
 
-ax.plot(vor.points[:,0], vor.points[:,1], '.')
+    for op in outer_points:
+        p = [nv for nv, ov in zip(new_vertices, outer_vertices) if op in ov]
+        vor.ridge_vertices = np.vstack([vor.ridge_vertices, p])
+        vor.ridge_points = np.vstack([vor.ridge_points, [op, -1]])
 
-for i in range(len(vor.vertices)):
-    ax.plot(vor.vertices[i, 0], vor.vertices[i,1], 'wo')
-    ax.text(vor.vertices[i, 0], vor.vertices[i,1]-1, i, color='r')
+    # Cycle through points
+    for point_idx in range(len(vor.points)):
+        region_idx = vor.point_region[point_idx]
+        segs = vor.ridge_vertices[(vor.ridge_points == point_idx).any(axis=1)]
 
-for i in range(len(vor.points)):
-    ax.text(vor.points[i, 0], vor.points[i,1], i)
+        for i in range(1, len(segs)):
+            # Put segments in correct order
+            idx = np.where([np.any(np.in1d(segs[i-1], s)) for s in segs[i:]])[0][-1]
+            segs[i:] = np.roll(segs[i:], -idx, axis=0)
 
+        # Extract vertices from segments
+        pts = segs.flatten()
+        _, i = np.unique(pts, return_index=True)
+        vor.regions[region_idx] = pts[np.sort(i)]
 
-ax = plt.axes()
-voronoi_plot_2d(vor, ax=ax)
-ax.set_xlim(-10, 10)
-ax.set_ylim(-10, 10)
+    return vor
 
+# Generate voroni diagrams
+points = np.random.random([10, 2])
+vor_a = Voronoi(points)
+vor_b = finite_voronoi(points, scale=1)
+
+# Get limits of vorionoi diagram
+x = vor_b.vertices[:, 0]
+y = vor_b.vertices[:, 1]
+
+vor = [vor_a, vor_b]
+lab = ['Infinite voronoi diagram', 'Finite voronoi diagram']
+
+fig, ax = plt.subplots(1, 2)
+for i in range(len(vor)):
+    voronoi_plot_2d(vor[i], ax=ax[i])
+    ax[i].set_title(lab[i])
+    ax[i].axis((x.min(), x.max(), y.min(), y.max()))
 plt.show()
-
-# attributes to add
-# vor.point_region     
-# vor.points           not needed
-# vor.regions          
-# vor.ridge_dict       
-# vor.ridge_points     done
-# vor.ridge_vertices   done
-# vor.vertices         done
-
-
-
-
